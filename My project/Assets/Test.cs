f4779814b4ce3d22e5b41d30e2951dbd25285eed
김민준 <kmj387100@naver.com>
2024-10-14 16:13:39 +0900

    asd

---
+++ My project/Assets/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HHHHHHHHHHHHHh");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("HHHHHHHHHHHHHh" + count++);
+    }
+}
