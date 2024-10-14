a8ddb0fca8f30212ed03df179282b75bb44b6f5f
김민준 <kmj387100@naver.com>
2024-10-14 17:32:01 +0900

    dfs

--- My project/Assets/Test.cs
+++ My project/Assets/Test.cs
@@ -12,7 +12,7 @@ public class Test : MonoBehaviour
         Debug.Log("HHHHHHHHHHHHHh");
         Debug.Log("HHHHHHHHHHHHHh");
         Debug.Log("HHHHHHHHHHHHHh");
- Debug.Log("HHㄴㅇ");
+        Debug.Log("HHㄴㅇ");
     }
 
     // Update is called once per frame
